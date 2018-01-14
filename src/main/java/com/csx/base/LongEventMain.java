1f1aa2b55531063ef69aec88b36a762d945f1932
chensongxian <1164077611@qq.com>
2018-01-14 21:22:48 +0800

    disruptor使用

--- src/main/java/com/csx/base/LongEventMain.java
+++ src/main/java/com/csx/base/LongEventMain.java
@@ -26,6 +26,14 @@ public class LongEventMain {
 
         //创建bufferSize ,也就是RingBuffer大小，必须是2的N次方
         int ringBufferSize = 1024 * 1024;
+        /**
+         //BlockingWaitStrategy 是最低效的策略，但其对CPU的消耗最小并且在各种不同部署环境中能提供更加一致的性能表现
+         WaitStrategy BLOCKING_WAIT = new BlockingWaitStrategy();
+         //SleepingWaitStrategy 的性能表现跟BlockingWaitStrategy差不多，对CPU的消耗也类似，但其对生产者线程的影响最小，适合用于异步日志类似的场景
+         WaitStrategy SLEEPING_WAIT = new SleepingWaitStrategy();
+         //YieldingWaitStrategy 的性能是最好的，适合用于低延迟的系统。在要求极高性能且事件处理线数小于CPU逻辑核心数的场景中，推荐使用此策略；例如，CPU开启超线程的特性
+         WaitStrategy YIELDING_WAIT = new YieldingWaitStrategy();
+         */
 
         //创建disruptor
         //1第一个参数为工厂类对象，用于创建一个个的LongEvent，LongEvent是实际的消费数据
