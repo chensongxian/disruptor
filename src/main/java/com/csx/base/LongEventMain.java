2680c57752d00ed59e7b37e66320a5e0293bd1fb
chensongxian <1164077611@qq.com>
2018-01-14 21:21:59 +0800

    disruptor使用

--- src/main/java/com/csx/base/LongEventMain.java
+++ src/main/java/com/csx/base/LongEventMain.java
@@ -1,9 +1,11 @@
 package com.csx.base;
 
+import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.YieldingWaitStrategy;
 import com.lmax.disruptor.dsl.Disruptor;
 import com.lmax.disruptor.dsl.ProducerType;
 
+import java.nio.ByteBuffer;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -25,8 +27,36 @@ public class LongEventMain {
         //创建bufferSize ,也就是RingBuffer大小，必须是2的N次方
         int ringBufferSize = 1024 * 1024;
 
+        //创建disruptor
+        //1第一个参数为工厂类对象，用于创建一个个的LongEvent，LongEvent是实际的消费数据
+        //2第二个参数为缓存区大小
+        //3第三个参数线程池进行Disruptor内部的数据接收处理调度
+        //4第四个参数ProducerType.SINGLE和ProducerType.MULTI
+        //5第五个参数是一种策略，WaitStrategy
         Disruptor<LongEvent> disruptor=new Disruptor<LongEvent>(eventFactory,ringBufferSize
                 ,executors, ProducerType.SINGLE,new YieldingWaitStrategy());
 
+        //连接消费事件方法
+        disruptor.handleEventsWith(new LongEventHandler());
+
+        disruptor.start();
+
+        //Disruptor的事件发布过程是一个两阶段提交的过程
+        //发布事件
+        RingBuffer<LongEvent> ringBuffer=disruptor.getRingBuffer();
+
+        LongEventProducer producer=new LongEventProducer(ringBuffer);
+
+        ByteBuffer byteBuffer=ByteBuffer.allocate(8);
+        for(long l=0;l<100;l++){
+            byteBuffer.putLong(0,l);
+            producer.onData(byteBuffer);
+        }
+        //关闭disruptor,方法会堵塞,直到所有的事件都得到处理
+        disruptor.shutdown();
+        //关闭disruptor使用的线程池
+        executors.shutdown();
     }
+
+
 }
--- src/main/java/com/csx/base/LongEventProducer.java
+++ src/main/java/com/csx/base/LongEventProducer.java
@@ -1,5 +1,9 @@
 package com.csx.base;
 
+import com.lmax.disruptor.RingBuffer;
+
+import java.nio.ByteBuffer;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -8,4 +12,26 @@ package com.csx.base;
  * @Date: 2018-01-14
  */
 public class LongEventProducer {
+    private final RingBuffer<LongEvent> ringBuffer;
+    public LongEventProducer(RingBuffer<LongEvent> ringBuffer) {
+        this.ringBuffer = ringBuffer;
+    }
+
+    public void onData(ByteBuffer bb){
+        //1. 可以把ringBuffer看做一个事件队列，那么next就是得到下面一个事件槽
+        long sequence=ringBuffer.next();
+        try {
+            //2. 用上面的索引取出一个空的事件用于填充（获取该序号对应的事件对象）
+            LongEvent event=ringBuffer.get(sequence);
+            //3. 获取要通过事件传递的业务数据
+            event.setValue(bb.getLong(0));
+        }finally {
+            //4. 发布事件
+            //注意，最后的ringBuffer.publish方法必须包含在finally中以确保必须得到调用
+            //如果某个请求的sequence未提交，将会堵塞后续的发布操作或者其他的producer
+            ringBuffer.publish(sequence);
+        }
+    }
+
+
 }
